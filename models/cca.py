36a177690fb20501ae148301801dc4eaeca80997
gkgo <91817417+gkgo@users.noreply.github.com>
2022-07-14 16:29:07 +0800

    Update cca.py

--- models/cca.py
+++ models/cca.py
@@ -72,16 +72,14 @@ class match_block(nn.Module):
                     kernel_size=1, stride=1, padding=0),
             bn(self.in_channels)
         )
-        nn.init.constant_(self.W[1].weight, 0)
-        nn.init.constant_(self.W[1].bias, 0)
+
 
         self.Q = nn.Sequential(
             conv_nd(in_channels=self.inter_channels, out_channels=self.in_channels,
                     kernel_size=1, stride=1, padding=0),
             bn(self.in_channels)
         )
-        nn.init.constant_(self.Q[1].weight, 0)
-        nn.init.constant_(self.Q[1].bias, 0)
+
 
         self.theta = conv_nd(in_channels=self.in_channels, out_channels=self.inter_channels,
                              kernel_size=1, stride=1, padding=0)
