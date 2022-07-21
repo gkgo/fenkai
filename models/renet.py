6e9a387b16d572f35289bfeee06832f498e44947
gkgo <91817417+gkgo@users.noreply.github.com>
2022-07-21 16:42:21 +0800

    Update renet.py

--- models/renet.py
+++ models/renet.py
@@ -44,9 +44,14 @@ class RENet(nn.Module):
         layers = list()
 
         if self.args.self_method == 'scr':
-            corr_block1 = SelfCorrelationComputation1(d_model=640, h=1)
+            # corr_block1 = SelfCorrelationComputation1(d_model=640, h=1)
             # corr_block = SelfCorrelationComputation(kernel_size=kernel_size, padding=padding)
             # self_block = SCR(planes=planes, stride=stride)
+            # corr_block2 = SelfCorrelationComputation6(in_planes=640, out_planes=640)
+            corr_block2 = SelfCorrelationComputation5(in_channels=640, out_channels=640)
+            # corr_block2 = SelfCorrelationComputation4(channel=640)
+            # corr_block2 = SelfCorrelationComputation3(in_channels=640)
+            # corr_block2 = SelfCorrelationComputation2(in_channels=640, out_channels=640, kernel_size=5)
         # elif self.args.self_method == 'sce':
         #     planes = [640, 64, 64, 640]
         #     self_block = SpatialContextEncoder(planes=planes, kernel_size=kernel_size[0])
@@ -60,7 +65,7 @@ class RENet(nn.Module):
             raise NotImplementedError
 
         if self.args.self_method == 'scr':
-            layers.append(corr_block1)
+            layers.append(corr_block2)
         #     layers.append(corr_block)
         # layers.append(self_block)
         return nn.Sequential(*layers)
@@ -93,7 +98,7 @@ class RENet(nn.Module):
         num_qry, way, H_s, W_s, H_q, W_q = corr4d.size()
 
         # corr4d refinement
-#         corr4d = self.cca_module(corr4d.view(-1, 1, H_s, W_s, H_q, W_q))
+        # corr4d = self.cca_module(corr4d.view(-1, 1, H_s, W_s, H_q, W_q))
         corr4d_s = corr4d.view(num_qry, way, H_s * W_s, H_q, W_q)  # 10，5，25，5，5
         corr4d_q = corr4d.view(num_qry, way, H_s, W_s, H_q * W_q)  # 10，5，5，5，25
 
@@ -305,10 +310,10 @@ class RENet(nn.Module):
         if self.args.self_method:
             identity = x  # (80,640,5,5)
             x = self.scr_module(x)
-            x = self.match_net1(x,identity)
+            # x = self.match_net1(x,identity)
 
-            # if self.args.self_method == 'scr':
-            #     x = x + identity   # 公式（2）
+            if self.args.self_method == 'scr':
+                x = x + identity   # 公式（2）
             x = F.relu(x, inplace=True)
 
         if do_gap:
